d389dd4ed7e7f5ad8b82b3b54a376e5c26392b1e
SebastianPintoSAPT <sebastianalbertopintotorres@gmail.com>
2026-02-23 12:35:44 -0500

    feat: implement Passenger model

--- src/main/java/edu/upb/model/Passenger.java
+++ src/main/java/edu/upb/model/Passenger.java
@@ -2,4 +2,38 @@ package edu.upb.model;
 
 public class Passenger {
 
+  private String id;
+  private String name;
+  private int age;
+
+  public Passenger(String id, String name, int age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    if (age < 0) throw new IllegalArgumentException("Age cannot be negative");
+    this.age = age;
+  }
 }
