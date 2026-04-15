31315aad3e6b8dc13f3ffceedd0e9e01f489dbea
Sebastian Pinto <sebastian.pinto@upb.edu.co>
2026-04-15 15:58:16 -0500

    feat: add PersistenceModule for saving/loading tickets using serialization

--- src/main/java/edu/upb/server/persistence/PersistenceModule.java
+++ src/main/java/edu/upb/server/persistence/PersistenceModule.java
@@ -1,5 +1,35 @@
 package edu.upb.server.persistence;
 
+import edu.upb.model.Ticket;
+import edu.sebsx.app.linkedlist.singly.SinglyLinkedList;
+import java.io.*;
+
 public class PersistenceModule {
-  
+
+  private static final String TICKETS_FILE = "tickets.dat";
+
+  public boolean saveTicket(Ticket ticket) {
+    SinglyLinkedList<Ticket> tickets = loadTickets();
+    tickets.add(ticket);
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(TICKETS_FILE))) {
+      oos.writeObject(tickets);
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public SinglyLinkedList<Ticket> loadTickets() {
+    File file = new File(TICKETS_FILE);
+    if (!file.exists()) {
+      return new SinglyLinkedList<>();
+    }
+    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+      return (SinglyLinkedList<Ticket>) ois.readObject();
+    } catch (Exception e) {
+      return new SinglyLinkedList<>();
+    }
+  }
 }
