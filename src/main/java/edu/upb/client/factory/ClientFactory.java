31d588aba4a64c5b0bd31e796ea76c64cf8cb153
Sebastian Pinto <sebastian.pinto@upb.edu.co>
2026-04-20 17:50:36 -0500

    refactor: adapt ClientFactory to new ClientController signature

--- src/main/java/edu/upb/client/factory/ClientFactory.java
+++ src/main/java/edu/upb/client/factory/ClientFactory.java
@@ -2,7 +2,6 @@ package edu.upb.client.factory;
 
 import edu.upb.client.controller.ClientController;
 import edu.upb.client.model.ClientModel;
-import edu.upb.client.view.PurchaseView;
 import edu.upb.common.Environment;
 
 public class ClientFactory {
@@ -11,23 +10,9 @@ public class ClientFactory {
   }
 
   public static ClientController create() {
-
-    Environment env = null;
-
-    try {
-      env = Environment.getInstance();
-    } catch (Exception e) {
-      System.err.println("Failed to initialize Environment: " + e.getMessage());
-    }
-
-    if (env == null) {
-      throw new IllegalStateException("Environment is not initialized");
-    }
-
+    Environment env = Environment.getInstance();
+    if (env == null) throw new IllegalStateException("Environment is not initialized");
     ClientModel model = new ClientModel(env.getIp(), env.getPort(), env.getServiceName());
-
-    PurchaseView view = new PurchaseView(model);
-
-    return new ClientController(model, view);
+    return new ClientController(model);
   }
 }
