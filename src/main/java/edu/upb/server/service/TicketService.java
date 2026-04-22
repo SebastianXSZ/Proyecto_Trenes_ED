d6c6afe4e670a7fc029b0c0bc77d95b638f45dc0
Sebastian Pinto <sebastian.pinto@upb.edu.co>
2026-04-22 15:28:05 -0500

    feat: implement CRUD methods for Train in TicketService

--- src/main/java/edu/upb/server/service/TicketService.java
+++ src/main/java/edu/upb/server/service/TicketService.java
@@ -6,6 +6,7 @@ import edu.upb.common.TicketInterface;
 import edu.upb.common.LoginDTO;
 import edu.upb.common.SaleDTO;
 import edu.upb.model.Ticket;
+import edu.upb.model.Train;
 import edu.upb.server.business.SalesManager;
 import edu.upb.server.business.SecurityModule;
 import edu.upb.server.persistence.PersistenceModule;
@@ -33,13 +34,16 @@ public class TicketService extends UnicastRemoteObject implements TicketInterfac
 
   @Override
   public boolean validate(Ticket ticket) throws RemoteException {
-    return persistenceModule.loadTickets().contains(ticket);
+    return persistenceModule.loadTickets()
+            .contains(ticket);
   }
 
   @Override
   public Ticket purchaseTicket(SaleDTO dto) throws RemoteException {
     Ticket ticket = salesManager.processTransaction(dto);
-    if (ticket != null) persistenceModule.saveTicket(ticket);
+    if (ticket != null) {
+      persistenceModule.saveTicket(ticket);
+    }
     return ticket;
   }
 
@@ -51,5 +55,25 @@ public class TicketService extends UnicastRemoteObject implements TicketInterfac
   @Override
   public String[] getStationNames() throws RemoteException {
     return new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"};
+  }
+
+  @Override
+  public void addTrain(Train train) throws RemoteException {
+    salesManager.addTrain(train);
+  }
+
+  @Override
+  public Train[] getAllTrains() throws RemoteException {
+    return salesManager.getAllTrains();
+  }
+
+  @Override
+  public void updateTrain(Train train) throws RemoteException {
+    salesManager.updateTrain(train);
+  }
+
+  @Override
+  public void deleteTrain(String trainId) throws RemoteException {
+    salesManager.deleteTrain(trainId);
   }
 }
